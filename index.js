05e9327b4ce933abf856ae5f48dbfbda4099341b
Soumya Adhya <soumyaadhya007@gmail.com>
2023-02-15 23:53:09 +0530

    add

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-function myFunction() {
-    var x = document.getElementById("nav-menu");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
+// function myFunction() {
+//     var x = document.getElementById("nav-menu");
+//     if (x.className === "topnav") {
+//         x.className += " responsive";
+//     } else {
+//         x.className = "topnav";
+//     }
+// }
 
-function Resume() {
-    console.log("resume");
-}
+// function Resume() {
+//     console.log("resume");
+// }
+
+var typed= new Typed(".variable", {
+    strings: ["Full Stack Web Developer", "Coder", "Problem Solver"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
