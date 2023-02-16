8c0f9b53177a2eba440eda4075ea8d6884953a7a
Soumya Adhya <soumyaadhya007@gmail.com>
2023-02-16 21:03:28 +0530

    add

--- index.js
+++ index.js
@@ -18,3 +18,11 @@ GitHubCalendar(".calendar", "SoumyaAdhya007", {
   },
 }).then((r) => console.log(r))
 .catch((err)=>console.log(err))
+
+document.getElementById("resume-button-1").onclick=()=>{
+    window.open("https://drive.google.com/file/d/1BPDySMDMXclvoj4oCfOHycLf5hceaZJa/view?usp=share_link");
+      }
+    
+      document.getElementById("resume-button-2").onclick=()=>{
+        window.open("https://drive.google.com/file/d/1BPDySMDMXclvoj4oCfOHycLf5hceaZJa/view?usp=share_link");
+      }
