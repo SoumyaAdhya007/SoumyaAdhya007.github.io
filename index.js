1458251e13200c1b446ee64c0b9a504e5c09133e
Soumya Adhya <soumyaadhya007@gmail.com>
2023-03-20 20:33:21 +0530

    LLD Completed

--- index.js
+++ index.js
@@ -9,10 +9,15 @@ GitHubCalendar(".calendar", "SoumyaAdhya007", {
   });
 
 
-  document.getElementById("resume-button-1").onclick=()=>{
-    window.open("https://drive.google.com/file/d/1BPDySMDMXclvoj4oCfOHycLf5hceaZJa/view?usp=share_link");
-      }
+  // document.getElementById("resume-button-1").onclick=()=>{
+  //   window.open("https://drive.google.com/file/d/1BPDySMDMXclvoj4oCfOHycLf5hceaZJa/view?usp=share_link");
+  //     }
     
-      document.getElementById("resume-button-2").onclick=()=>{
-        window.open("https://drive.google.com/file/d/1BPDySMDMXclvoj4oCfOHycLf5hceaZJa/view?usp=share_link");
-      }
+  //     document.getElementById("resume-button-2").onclick=()=>{
+  //       window.open("https://drive.google.com/file/d/1BPDySMDMXclvoj4oCfOHycLf5hceaZJa/view?usp=share_link");
+  //     }
+  let resume1 = document.getElementById("resume-link-1").addEventListener("click",opendrive);
+  let resume2 = document.getElementById("resume-link-2").addEventListener("click",opendrive);
+function opendrive(){
+  window.open((href ="https://drive.google.com/file/d/1BPDySMDMXclvoj4oCfOHycLf5hceaZJa/view?usp=share_link"));
+ }
